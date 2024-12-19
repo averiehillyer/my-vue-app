97d9f829dea803c3feb9cbd0017441f2bd4c9661
Averie Hillyer <hilly030@umn.edu>
2024-12-18 22:43:01 -0600

    Added additional data

--- src/MOCK_DATA.js
+++ src/MOCK_DATA.js
@@ -7,7 +7,8 @@ const data = [
         "class": "N/A",
         "image": "AmIAliveListing",
         "description": "This digital poster print was inspired by the song 'Yeah (bonus)' by Mac Miller on the album 'Faces'. I took elements of the original front cover, and applied them to a song lyric from the song. The song discusses a battle within one's identity so I wanted to be able to reflect that abstract emotion into the poster.",
-        "professor": "N/A"
+        "professor": "N/A",
+        "application": "Adobe Photoshop, Illustrator, InDesign"
     },
     {
         "id": "2",
@@ -17,7 +18,8 @@ const data = [
         "class": "N/A",
         "image": "channel-orange-listing",
         "description": "I was inspired by a photo of an orange that I saw with super detailed water droplets. It immediately reminded me of the album by Frank Ocean, 'Channel Orange'. I wanted to combine the HD orange with the abstract CD shape to create a balance and represent the album itself.",
-        "professor": "N/A"
+        "professor": "N/A",
+        "application": "Adobe Photoshop & Illustrator"
     },
     {
         "id": "3",
@@ -27,7 +29,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Daniel-caesar-listing",
         "description": "Daniel Caesar is one of my favorite artists. His smooth tone creates a sense of euphoria in his RnB songs and I wanted to reflect that in this poster. I used elements of his album cover, and paired it with a simple and clean text displaying his name. I thought the orange color paired nicely with the off-white/cream background.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },
     {
         "id": "4",
@@ -37,7 +40,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Faiyaz-listing",
         "description": "Brent Faiyaz is an RnB artist that inspired a lot of my artwork. I found a snapshot of his signature and paired it with an image of his side profile. I wanted to add some movement to the piece to I added some dropping paint effects in the text, as well as some motion lines at the bottom to keep the piece balanced.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },
     {
         "id": "5",
@@ -47,7 +51,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Frank-ocean-listing",
         "description": "This is a poster set for Frank Ocean. The poster features a brief overview of Frank's discography - highlighting some of his most popular songs. The poster also features a song lyric from the song 'White Ferrari', along with his birth year in the background. The corners are decorated with an emblem as well as two Frank album titles.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },
     {
         "id": "6",
@@ -57,7 +62,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Kill-bill-listing",
         "description":"'Kill Bill' is one of SZA's most popular songs off of her most recent album - S.O.S. I chose to use red colors to convey the feeling of the song, and kept it simple in the background since I feel like the image speaks fairly strongly. ",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     }, {
         "id": "7",
         "name": "Nostalgia, Ultra - Frank Ocean ",
@@ -66,7 +72,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Nostalgia-ultra-listing",
         "description":"Nostalgia, Ultra is a mixtape recorded by Frank Ocean.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     }, {
         "id": "8",
         "name": "SiR",
@@ -75,7 +82,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Orange-SiR",
         "description":"SiR is an RnB artist that I listen to very frequently. His music is very laid back and chill which I thought would be valuable to mimic. The large playful type is layered in the back to add some depth to the image, and is colored to fit within the color scheme of the couch.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     }, {
         "id": "9",
         "name": "SZA SOS",
@@ -84,7 +92,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "SZA-listing",
         "description":"S.O.S is the most recent album released by the artist SZA in 2022. The RnB album includes lots of self-reflection which I thought was very well represented on the album cover. I cut out SZA from the cover art and placed her on a blank background with her name written behind it. I thought that the bright orange complimented the turquoise in the waters really well and the simplicity helped keep the serene feeling that both the album and the ocean bring.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     }, {
         "id": "10",
         "name": "What You Heard - Sonder",
@@ -93,7 +102,8 @@ const data = [
         "class": "Color & Form in Surface Design",
         "image": "What-you-heard-listing",
         "description":"Sonder is a band comprised of RnB singer Brent Faiyaz, Atu, and Dpat. The word sonder is such a beautiful word that I wanted to highlight along with an adaptation of one of their EP covers - 'What You Heard'. I took the cover art and simplified it in order to create a layer that was able to be screenprinted. I chose to do all black with a pop of red because I felt that it helped represent the feeling of sonder and how it can be both isolating (red) and all consuming (black). I added a distressed typeface to compliment the eroded eye image above. I wanted the text to blend in with the image so I kept it black and inverted the type when it is layered over the image.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     }, {
         "id": "11",
         "name": "Zine Spread - Legends of RnB",
@@ -102,7 +112,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-1",
         "description":"This is a spread from a zine I produced in my class for a final project. Being that music and art are two of my favorite subjects, I wanted to combine the two and make visually appealing spreads on artists that I love. I did not think that it would be fair to address the artists of RnB now without acknowledging the artists that paved the way for the genre today. The rnb text is filled with numerous legends in the music industry that have made RnB what it is today. The theme colors for this zine were blue, orange, and black so I thought the Fugees' cover art fit the color palette perfectly.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "12",
         "name": "Zine Spread - History of RnB",
@@ -111,7 +122,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-2",
         "description":"Going further into acknowledging musicians in the past, I also wanted to acknowledge the past of the RnB genre as a whole. There is a rich history there and a very interesting evolution from what it originated from into what it is today.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "13",
         "name": "Zine Spread - 2024 Intro",
@@ -120,7 +132,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-3",
         "description":"This is a spread transitioning the zine into modern day musicians. I touched on the history of RnB and the musicians that came before, but now I wanted to make it clear that I was talking about present day.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "14",
         "name": "Zine Spread - Brent Faiyaz Intro",
@@ -129,7 +142,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-4",
         "description":"This is a spread used to introduce the artist Brent Faiyaz. I featured his popular songs, as well as some album artwork and an image of the artist himself.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Illustrator"
     },{
         "id": "15",
         "name": "Zine Spread - Brent Faiyaz",
@@ -138,7 +152,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-5",
         "description":"This is a spread giving a biography on the artist Brent Faiyaz - written by an outside source - formatted in a fun way featuring quotes and another image of the artist.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "16",
         "name": "Zine Spread - Sonder Intro",
@@ -147,7 +162,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-6",
         "description":"This is a spread introducing the band Sonder - comprised of Atu, Dpat, and Brent Faiyaz. The spread features an image of the group. as well as highlighting some of their most popular songs.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "17",
         "name": "Zine Spread - Sonder",
@@ -156,7 +172,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-7",
         "description":"This is a spread giving background on Sonder - written by an outside source. The pages feature the year the group was formed, and an image of the group as well.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Illustrator"
     },{
         "id": "18",
         "name": "Zine Spread - Frank Ocean Intro",
@@ -165,7 +182,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-8",
         "description":"This spread introduces Frank Ocean. It features his birth year, some of his most popular songs, as well as a song lyric from the song 'White Ferrari'.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Illustrator"
     },{
         "id": "19",
         "name": "Zine Spread",
@@ -174,7 +192,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-9",
         "description":"This spread is about Frank Ocean and giving a more in depth look into his life. The article was written by an outside source. The spread aligns with the same distressed orange theme that is seen throughout the zine.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Photoshop & Illustrator"
     },{
         "id": "20",
         "name": "Zine Spread - SZA",
@@ -183,7 +202,8 @@ const data = [
         "class": "Design & Discontents",
         "image": "Spread-10",
         "description":"This is a spread highlighting the artist SZA. Her name is cut out of her album art work from her most recent album 'S.O.S' and paired with some of her most popular tracks.",
-        "professor": "Daniel Jasper"
+        "professor": "Daniel Jasper",
+        "application": "Adobe Illustrator"
     }]
 
 export default {data}
